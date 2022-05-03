2b4847cdfe7514c86a2c087c05f3747c821ca9f3
SH <https://github.com/miracleSH>
2022-05-03 14:44:27 +0900

    iron-session을 이용한 쿠키 저장

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -1,11 +1,11 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 
 export interface ResponseType {
   ok: boolean
 }
 
-export default function withHandler(httpMethodType: 'GET' | 'POST' | 'PUT' | 'DELETE', fn: (req: NextApiRequest, res: NextApiResponse) => void) {
-  return async function (req: NextApiRequest, res: NextApiResponse) {
+export default function withHandler(httpMethodType: 'GET' | 'POST' | 'PUT' | 'DELETE', fn: (req: NextApiRequest, res: NextApiResponse) => void) : NextApiHandler<any> {
+  return async function (req: NextApiRequest, res: NextApiResponse) : Promise<any> {
     if(req.method !== httpMethodType){
       return res.status(405).end()
     }
--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,15 +1,29 @@
 import client from '@libs/server/client'
-import withHandler, {ResponseType} from '@libs/server/withHandler'
+import { withIronSessionApiRoute } from 'iron-session/next'
+import withHandler, { ResponseType } from '@libs/server/withHandler'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-const confirm = async (req: NextApiRequest, res: NextApiResponse<ResponseType>) => {
+async function confirm (req: NextApiRequest, res: NextApiResponse<ResponseType>) {
   const { token } = req.body
-  // let  dbToken = await client.token.findUnique({
-  //   where : {
-      
-  //   }
-  // })
-  console.log("token : " + token)
-  res.status(200).end()
+  let result = await client.token.findUnique({
+     where : {
+        payload : token
+     },
+     //include : {user : true} // --> token과 user가 연관관계에 있기 때문에 token과 연결된 user 객체를 불러올 수 있음.
+   })
+   if(result){
+     req.session.user = {
+       id : result?.userId
+     }
+     await req.session.save()
+     
+     res.status(200).json({ok : true})
+   }else{
+     res.status(404).json({ok : false})
+   }
+   console.log(req.session)
 }
-export default withHandler('POST', confirm)
+export default withIronSessionApiRoute(withHandler('POST', confirm), {
+  cookieName : "carrotSession",
+  password : "789456123asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasfsafasdfas44564564s65d4a6s4d56s4d654a56sd4as564d654"
+})
